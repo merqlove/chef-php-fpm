6d4d689b9782d0cfc6b240131f5d2dc33acb45d1
Alexander Merkulov <sasha@merqlove.ru>
2014-07-27 08:41:22 +0400

    try utf via other recipe

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,7 +1,3 @@
-if RUBY_VERSION =~ /1.9/
-  Encoding.default_external = Encoding::UTF_8
-  Encoding.default_internal = Encoding::UTF_8
-end
 #
 # Author::  Seth Chisamore (<schisamo@opscode.com>)
 # Cookbook Name:: php-fpm
