13acf041e7f671a16d56f35e9b7e7726242c1d16
Alexander Merkulov <sasha@merqlove.ru>
2014-07-27 08:33:49 +0400

    fix ascii bug

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,3 +1,7 @@
+if RUBY_VERSION =~ /1.9/
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
 #
 # Author::  Seth Chisamore (<schisamo@opscode.com>)
 # Cookbook Name:: php-fpm
